6169c9c32c8ca4ae6a0570e0972ad05ac3e4e340
Ash <138498235+cyberashsec@users.noreply.github.com>
2023-12-19 04:51:21 -0600

    Update Calculator.py
    Fixing a little bug and making it so the program doesn't instantly exit in Command Prompt

--- Calculator.py
+++ Calculator.py
@@ -3,12 +3,13 @@ num2 = input("Enter your second number: ")
 operation = input("What type of operation would you like to do? ")
 
 if operation == "+":
-    result = float(num1) + float(num2)
+    result = float(num1)+float(num2)
 elif operation == "-":
-    result = float(num1) - float(num2)
+    result = float(num1)-float(num2)
 elif operation == "*":
-    result = float(num1) * float(num2)
+    result = float(num1)*float(num2)
 elif operation == "/":
-    result = float(num1) / float(num2)
+    result = float(num1)/float(num2)
 
-print(round(result), 1)
+print(round(result, 1))
+print(input(""))
